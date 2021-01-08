173db3c5ce6b20e17dc2fb8473e3acbf5aeed8b5
AA_Arnold <amasoharnold@gmail.com>
2021-01-08 04:39:42 -1100

    added banner in hero section

--- src/components/Hero/Hero.component.jsx
+++ src/components/Hero/Hero.component.jsx
@@ -1,14 +1,13 @@
-
-
 export default function Hero(){
     return(
-     <main className='bg-purple-700'>
-         <div className='text-white'>
-            <span className='block'>Hi ! 👋,</span>
-            <span className='block'>I'm Amasoh.A.Arnold,</span>
+     <main className='bg-purple-700 flex justify-between'>
+         <div className='text-white text-4xl font-bold w-auto p-6 '>
+            <span className='block mb-4'>Hi ! 👋,</span>
+            <span className='block mb-4'>I'm Amasoh.A.Arnold,</span>
             <span className='block'>Software Developer</span>
          </div>
-         <div>
+         <div className='p-6'>
+         <img src='hero.svg' alt="An SVG of a male using a laptop" />
          </div>
      </main>
     )
