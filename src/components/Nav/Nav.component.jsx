31b11e8447da40620f535841ed9ca4433a5c5939
AA_Arnold <amasoharnold@gmail.com>
2021-01-14 06:18:19 -1100

    added svg for sun and moon

--- src/components/Nav/Nav.component.jsx
+++ src/components/Nav/Nav.component.jsx
@@ -1,5 +1,7 @@
 import Link from 'next/link'
 import styles from './Nav.module.css'
+import Moon from '../../../assets/moon.svg'
+import Sun from '../../../assets/sun.svg'
 
 export default function Nav(){
     return(
@@ -19,20 +21,52 @@ export default function Nav(){
                             <li className="inline-block text-lg sm:text-base">
                                 <button aria-label="Enable light mode" className="text-orange-400 transition-colors duration-300 ease-linear focus:outline-none">
                                     <span className="hidden">
-                                        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="moon" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="svg-inline--fa fa-moon fa-w-16">
-                                            <path fill="currentColor" d="M283.211 512c78.962 0 151.079-35.925 198.857-94.792 7.068-8.708-.639-21.43-11.562-19.35-124.203 23.654-238.262-71.576-238.262-196.954 0-72.222 38.662-138.635 101.498-174.394 9.686-5.512 7.25-20.197-3.756-22.23A258.156 258.156 0 0 0 283.211 0c-141.309 0-256 114.511-256 256 0 141.309 114.511 256 256 256z" className="">
-                                            </path>
-                                        </svg>
+                                        <Moon/>
                                     </span> 
                                     
                                     <span className="block">
-                                        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="sun" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="svg-inline--fa fa-sun fa-w-16"><path fill="currentColor" d="M256 160c-52.9 0-96 43.1-96 96s43.1 96 96 96 96-43.1 96-96-43.1-96-96-96zm246.4 80.5l-94.7-47.3 33.5-100.4c4.5-13.6-8.4-26.5-21.9-21.9l-100.4 33.5-47.4-94.8c-6.4-12.8-24.6-12.8-31 0l-47.3 94.7L92.7 70.8c-13.6-4.5-26.5 8.4-21.9 21.9l33.5 100.4-94.7 47.4c-12.8 6.4-12.8 24.6 0 31l94.7 47.3-33.5 100.5c-4.5 13.6 8.4 26.5 21.9 21.9l100.4-33.5 47.3 94.7c6.4 12.8 24.6 12.8 31 0l47.3-94.7 100.4 33.5c13.6 4.5 26.5-8.4 21.9-21.9l-33.5-100.4 94.7-47.3c13-6.5 13-24.7.2-31.1zm-155.9 106c-49.9 49.9-131.1 49.9-181 0-49.9-49.9-49.9-131.1 0-181 49.9-49.9 131.1-49.9 181 0 49.9 49.9 49.9 131.1 0 181z" className="">
-                                            </path>
-                                        </svg>
+                                        <Sun/>
                                     </span>
                                </button>
                             </li>
                             
+                            <li className="hidden sm:inline-block">
+                                <Link href='/'>
+                                    <a href="" class="blog border-b-2 font-bold border-transparent border-b-transparent text-white hover:border-light-secondary dark:text-dark-primary dark-hover:border-dark-secondary text-base transition-all transition-duration-300 transition duration-500 ease-in-out">
+                                       Home
+                                    </a>
+                                </Link>
+                                <Link href='/'>
+                                    <a href="" class="blog border-b-2 font-bold border-transparent border-b-transparent text-white hover:border-light-secondary dark:text-dark-primary dark-hover:border-dark-secondary text-base transition-all transition-duration-300 transition duration-500 ease-in-out">
+                                       About
+                                    </a>
+                                </Link>
+                                <Link href='/'>
+                                    <a href="" class="blog border-b-2 font-bold border-transparent border-b-transparent text-white hover:border-light-secondary dark:text-dark-primary dark-hover:border-dark-secondary text-base transition-all transition-duration-300 transition duration-500 ease-in-out">
+                                       SKills
+                                    </a>
+                                </Link>
+                                <Link href='/'>
+                                    <a href="" class="blog border-b-2 font-bold border-transparent border-b-transparent text-white hover:border-light-secondary dark:text-dark-primary dark-hover:border-dark-secondary text-base transition-all transition-duration-300 transition duration-500 ease-in-out">
+                                       Blog
+                                    </a>
+                                </Link>
+                                <Link href='/'>
+                                    <a href="" class="blog border-b-2 font-bold border-transparent border-b-transparent text-white hover:border-light-secondary dark:text-dark-primary dark-hover:border-dark-secondary text-base transition-all transition-duration-300 transition duration-500 ease-in-out">
+                                       Project
+                                    </a>
+                                </Link>
+                                <Link href='/'>
+                                    <a href="" class="blog border-b-2 font-bold border-transparent border-b-transparent text-white hover:border-light-secondary dark:text-dark-primary dark-hover:border-dark-secondary text-base transition-all transition-duration-300 transition duration-500 ease-in-out">
+                                       Blog
+                                    </a>
+                                </Link>
+                                <Link href='/'>
+                                    <a href="" class="blog border-b-2 font-bold border-transparent border-b-transparent text-white hover:border-light-secondary dark:text-dark-primary dark-hover:border-dark-secondary text-base transition-all transition-duration-300 transition duration-500 ease-in-out">
+                                       Contact
+                                    </a>
+                                </Link>
+                            </li>
                         </ul>
                     </div>
             </nav>
