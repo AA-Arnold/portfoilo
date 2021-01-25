09e7f94becf15bfd73a6b03e3a0ed1e25b408a4c
AA_Arnold <amasoharnold@gmail.com>
2021-01-25 04:21:57 -1100

    added animation to about me card

--- src/components/About/About.component.jsx
+++ src/components/About/About.component.jsx
@@ -1,9 +1,13 @@
+import styles from './about.module.css'
+
 export default function About(){
     return(
      <div className='bg-white dark:bg-purple-pry'>
          <div className='flex justify-center text-black dark:text-white text-3xl font-bold mb-8 rounded-lg'>About</div>
          <div className='flex '>
-             <div className='w-52 h-52 bg-white'></div>
+             <div className={`w-52 h-52 shadow-lg p-8 rounded-lg bg-white ${styles.animate}`}>
+                 <img src='pin.png' alt='pin' />
+             </div>
              <div className='text-purple-txt w-52 ml-5'>
                  Hi i'm Amasoh Arnold, a software developer 
                  with background in computer science with a focus 
