2f85855e452a75ffdf797e40ec9f7846739739d2
AA_Arnold <amasoharnold@gmail.com>
2021-02-11 11:26:50 -1100

    modified about section

--- src/components/About/About.component.jsx
+++ src/components/About/About.component.jsx
@@ -8,7 +8,7 @@ export default function About(){
 
              {/*------------------------------------- Left ----------------------------------------- */}
 
-             <div className={`w-2/4  shadow-lg p-5 rounded-lg bg-purple-sec ml-5 h-2/4 ${styles.animate} lg:w-2/6`}>
+             <div className={`w-2/4  shadow-lg p-5 rounded-lg bg-purple-sec ml-5 h-2/4 ${styles.animate} lg:w-1/4`}>
                  <img src='pin.png' alt='pin' className={styles.pin} />
                  <div className='flex justify-center'>
                      <img src='arnold.jpeg' alt='image of Arnold Amasoh' className='rounded-lg' />
@@ -28,14 +28,14 @@ export default function About(){
              {/*------------------------------------------ Right ----------------------------------- */}
 
 
-             <div className='text-purple-txt w-2/4 ml-5 lg:w-4/6 lg:pl-14 lg:hidden md:hidden'>
-                 Hi i'm Amasoh Arnold, a software developer 
-                 with background in computer science with a focus 
-                 on building fast, responsive, dynamic and intuitive 
-                 user experience and interfaces for web or mobile 
+             <div className='text-purple-txt text-xl w-2/4 ml-5 md:hidden tracking-wider'>
+                    <span className='block '>Hello I'm Arnold</span>
+                    <span className='block  mt-3'>👈 That's me</span>  
+                    <span className='block  mt-3 mb-3'>swinging around</span> 
+                    <span className='leading-loose'>waiting for you to connect with me cheers</span>   
              </div>
 
-             <div className='hidden md:block text-purple-txt w-2/4 ml-5 lg:w-4/6 lg:pl-14'>
+             <div className='hidden md:block tracking-wider leading-relaxed text-purple-txt w-2/4 ml-5 lg:w-4/6 lg:pl-14'>
                  Hi i'm Amasoh Arnold, a software developer 
                  with background in computer science with a focus 
                  on building fast, responsive, dynamic and intuitive 
@@ -50,16 +50,16 @@ export default function About(){
                 using a particular tool 
              </div>
          </div>
-               <div className='text-purple-txt md:hidden'>
-                    Hi i'm Amasoh Arnold, a software developer 
-                    with background in computer science with a focus 
-                    on building fast, responsive, dynamic and intuitive 
-                    user experience and interfaces for web or mobile
+               <div className='text-purple-txt p-4 leading-relaxed  md:hidden'>
+                    Alright adding in a little info. I'm a software developer 👨‍💻
+                    with background in computer science 🤖 with a focus 
+                    on building fast 🚀, responsive 🖇, dynamic and intuitive 
+                    user experience 🎢 and interfaces for web or mobile
                     i'm based in Lagos, Nigeria. I'm also a certified
-                    web designer from 'HiiT' and I've taken numerous graphic
-                    design courses that has helped me so much in coming up 
-                    withgreat user interfaces I'm very curious about how 
-                    things work which pushes me to explore new technologies
+                    web designer 🧾 and I've taken numerous graphic
+                    design 👨‍🎨 courses that has helped me so much in coming up 
+                    withgreat user interfaces I'm very curious 🧐 about how 
+                    things work which pushes me to explore new technologies ⚗
                     that i find intriuging that is taylored to my domain 
                     helping me gain insights on merits and demerits of 
                     using a particular tool 
