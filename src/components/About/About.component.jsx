6d8e8f96e84fa09f3b29f07d57a10ef75593ea6c
AA_Arnold <amasoharnold@gmail.com>
2021-02-12 11:42:57 -1100

    modified picture frame to change color when toggled

--- src/components/About/About.component.jsx
+++ src/components/About/About.component.jsx
@@ -8,14 +8,14 @@ export default function About(){
 
              {/*------------------------------------- Left ----------------------------------------- */}
 
-             <div className={`w-2/4  shadow-lg p-5 rounded-lg bg-purple-sec ml-5 h-2/4 ${styles.animate} lg:w-1/4`}>
+             <div className={`w-2/4  shadow-lg p-5 rounded-lg bg-green-high dark:bg-purple-sec ml-5 h-2/4 ${styles.animate} lg:w-1/4`}>
                  <img src='pin.png' alt='pin' className={styles.pin} />
                  <div className='flex justify-center'>
                      <img src='arnold.jpeg' alt='image of Arnold Amasoh' className='rounded-lg' />
                  </div>
                  <div className='mt-4'>
-                     <p className='text-purple-high text-center'>Let's connect</p>
-                     <ul className='text-purple-txt text-center'>
+                     <p className='text-black dark:text-purple-high text-center'>Let's connect</p>
+                     <ul className='text-white dark:text-purple-txt text-center'>
                          <li>Gmail</li>
                          <li>LinkedIn</li>
                          <li>Github</li>
@@ -28,14 +28,14 @@ export default function About(){
              {/*------------------------------------------ Right ----------------------------------- */}
 
 
-             <div className='text-purple-txt text-xl w-2/4 ml-5 md:hidden tracking-wider'>
+             <div className='text-black dark:text-purple-txt text-xl w-2/4 ml-5 md:hidden tracking-wider'>
                     <span className='block '>Hello I'm Arnold</span>
                     <span className='block  mt-3'>👈 That's me</span>  
                     <span className='block  mt-3 mb-3'>swinging around</span> 
                     <span className='leading-loose'>waiting for you to connect with me cheers</span>   
              </div>
 
-             <div className='hidden md:block tracking-wider leading-relaxed text-purple-txt w-2/4 ml-5 lg:w-4/6 lg:pl-14'>
+             <div className='hidden md:block tracking-wider leading-relaxed text-black dark:text-purple-txt w-2/4 ml-5 lg:w-4/6 lg:pl-14'>
                  Hi i'm Amasoh Arnold, a software developer 
                  with background in computer science with a focus 
                  on building fast, responsive, dynamic and intuitive 
@@ -50,7 +50,7 @@ export default function About(){
                 using a particular tool 
              </div>
          </div>
-               <div className='text-purple-txt p-4 leading-relaxed  md:hidden'>
+               <div className='text-black dark:text-purple-txt p-4 leading-relaxed  md:hidden'>
                     Alright adding in a little info. I'm a software developer 👨‍💻
                     with background in computer science 🤖 with a focus 
                     on building fast 🚀, responsive 🖇, dynamic and intuitive 
