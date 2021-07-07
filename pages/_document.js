b7757483fc335c7e01b85857fc8bc133eb07fdbf
AA_Arnold <amasoharnold@gmail.com>
2021-07-06 21:05:35 -1100

    added option of falling back to key if not yet in production

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-
+import { GA_TRACKING_ID } from '../lib/gtag'
 class MyDocument extends Document {
     render() {
         return (
@@ -9,7 +9,7 @@ class MyDocument extends Document {
                     {/* Global Site Tag (gtag.js) - Google Analytics */}
                     <script
                         async
-                        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+                        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
                     />
                     <script
                         dangerouslySetInnerHTML={{
@@ -17,7 +17,7 @@ class MyDocument extends Document {
                                         window.dataLayer = window.dataLayer || [];
                                         function gtag(){dataLayer.push(arguments);}
                                         gtag('js', new Date());
-                                        gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+                                        gtag('config', '${GA_TRACKING_ID}', {
                                         page_path: window.location.pathname,
                                         });
                                     `,
