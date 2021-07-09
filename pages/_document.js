bf7c5cdb2392091842622ff08851dad2530f412f
AA_Arnold <amasoharnold@gmail.com>
2021-07-08 23:03:34 -1100

    added og properties and twitter meta descripton for better represntation on social media

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,17 @@ class MyDocument extends Document {
         return (
             <Html lang='eg'>
                 <Head>
+                    <link rel="canonical" href='https://aaatechie.com' />
                     <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+                    <meta property="og:url" content="https://aaatechie.com" />
+                    <meta property="og:title" content="When Great Minds Don’t Think Alike" />
+                    <meta property="og:description" content="A software developer 👨‍💻 with Bsc in Computer Science who specializes in creating and designing intuitive user experience and interfaces for web or mobile" />
+                    <meta property="og:image" content="https://avatars.githubusercontent.com/u/53374970?v=4?s=400" />
+
+                    <meta name="twitter:image:src" content="https://avatars.githubusercontent.com/u/53374970?v=4?s=400" />
+                    <meta name="twitter:card" content="summary" />
+                    <meta name="twitter:title" content="Arnold Amasoh" />
+                    <meta name="twitter:description" content="A software developer 👨‍💻 with Bsc in Computer Science who specializes in creating and designing intuitive user experience and interfaces for web or mobile" />
                     {/* Global Site Tag (gtag.js) - Google Analytics */}
                     <script
                         async
